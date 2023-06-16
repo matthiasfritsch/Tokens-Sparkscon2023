2add1a8fa2e08cf83144730e1feab053da770f5a
Matthias Fritsch <info@mfritsch.de>
2023-06-16 15:36:46 +0200

    Update build.js

--- build.js
+++ build.js
@@ -47,7 +47,7 @@ console.log('Build started...');
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-['base', 'light', 'dark', 'component'].map(function (theme) {
+['global', 'light', 'dark', 'component'].map(function (theme) {
 
     console.log('\n==============================================');
     console.log(`\nProcessing: [${theme}]`);
